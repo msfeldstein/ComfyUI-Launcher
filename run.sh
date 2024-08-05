6786bf7e0651b7d2edcceb08363a7e59f7b2da55
Michael Feldstein <msfeldstein@gmail.com>
2024-08-05 13:58:31 -0700

    Dont change virtualenv if you're already in one

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-if [ ! -d "venv" ]; then
-  echo "Creating virtual environment for ComfyUI Launcher..."
-  echo
-  echo
-  python3 -m venv venv
-  echo
-  echo
+if [[ -z "$VIRTUAL_ENV" ]]; then
+  if [ ! -d "venv" ]; then
+    echo "Creating virtual environment for ComfyUI Launcher..."
+    echo
+    echo
+    python3 -m venv venv
+    echo
+    echo
+  fi
+  . venv/bin/activate
 fi
-
 echo "Installing required packages..."
 echo
 echo
-. venv/bin/activate
 pip install -r requirements.txt
 
 echo
